8b588c7fc3c1272261bb548813231e39fd6bebed
Jonathan <jonathanmlaliberte@gmail.com>
2022-03-09 17:33:32 +0100

    calculate station distance

---
+++ main.py
@@ -0,0 +1,23 @@
+from scipy.spatial import distance
+import numpy
+
+
+stations = numpy.array([
+    [0, 0, 9],
+    [20, 20, 6],
+    [10, 0, 12],
+    [5, 5, 13],
+    [99, 25, 2],
+])
+devices = numpy.array([[0, 0],
+                    [100, 100],
+                    [15, 10],
+                    [18, 18],
+                    [13, 13],
+                    [25, 99]])
+
+
+def calculate_station_distance(a, b):
+    return distance.euclidean(a, b)
+
+
